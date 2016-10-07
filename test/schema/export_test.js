04a72d33b3f5cb8fcd15f3f0a1dfc6b2ebc875a8
Chris Moesel <cmoesel@mitre.org>
2016-10-06 21:33:11 -0400

    Updated models and test fixtures
    Updated the models to reflect the changes to the SHR grammar.
    
    Updated the test fixtures to reflect the changes to the SHR grammar, and updated the tests to correctly reference the new fixtures.  Did NOT update test logic to use new models -- it still operates against old model definitions.  Did NOT update any translator logic either.

--- test/schema/export_test.js
+++ test/schema/export_test.js
@@ -1,21 +1,12 @@
 const {expect} = require('chai');
 const fs = require('fs');
 const {exportToSchemas} = require('../../lib/schema/export');
-const {Namespace, DataElement, Entry, Identifier, QuantifiedIdentifier, PrimitiveIdentifier} = require('../../lib/models');
+const {Namespace, DataElement, Identifier, QuantifiedIdentifier, PrimitiveIdentifier} = require('../../lib/models');
 
 describe('#exportToSchemas()', () => {
-  it('should correctly export a simple entry', () => {
-    let expectedSchema = importFixture('simpleEntry');
-    let [ns, entry] = createNameSpaceAndEntry('shr.test', 'Simple');
-    entry.description = 'It is a simple entry';
-    entry.addAnswer(new PrimitiveIdentifier('date'));
-    let schema = exportToSingleSchema(ns);
-    expect(schema).to.eql(expectedSchema);
-  });
-
   it('should correctly export a simple data element', () => {
-    let expectedSchema = importFixture('simpleDataElement');
-    let [ns, de] = createNameSpaceAndDE('shr.test', 'simple');
+    let expectedSchema = importFixture('SimpleDataElement');
+    let [ns, de] = createNameSpaceAndDE('shr.test', 'Simple');
     de.description = 'It is a simple data element';
     de.addAnswer(new PrimitiveIdentifier('string'));
     let schema = exportToSingleSchema(ns);
@@ -23,34 +14,34 @@ describe('#exportToSchemas()', () => {
   });
 
   it('should correctly export a coded data element', () => {
-    let expectedSchema = importFixture('codedDataElement');
-    let [ns, de] = createNameSpaceAndDE('shr.test', 'coded');
+    let expectedSchema = importFixture('CodedDataElement');
+    let [ns, de] = createNameSpaceAndDE('shr.test', 'Coded');
     de.description = 'It is a coded data element';
     de.addAnswer(new PrimitiveIdentifier('code'));
-    de.valueset = 'http://standardhealthrecord.org/test/vs/coded';
+    de.valueset = 'http://standardhealthrecord.org/test/vs/Coded';
     let schema = exportToSingleSchema(ns);
     expect(schema).to.eql(expectedSchema);
   });
 
   it('should correctly export a choice data element', () => {
-    let expectedSchema = importFixture('choiceDataElement');
-    let [ns, de] = createNameSpaceAndDE('shr.test', 'choice');
+    let expectedSchema = importFixture('ChoiceDataElement');
+    let [ns, de] = createNameSpaceAndDE('shr.test', 'Choice');
     de.description = 'It is a data element with a choice';
     de.addAnswer(new PrimitiveIdentifier('date'));
-    de.addAnswer(new Identifier('other.ns', 'period'));
-    de.addAnswer(new Identifier('shr.test', 'simple'));
+    de.addAnswer(new Identifier('other.ns', 'Period'));
+    de.addAnswer(new Identifier('shr.test', 'Simple'));
     let schema = exportToSingleSchema(ns);
     expect(schema).to.eql(expectedSchema);
   });
 
-  it('should correctly export a composition', () => {
-    let expectedSchema = importFixture('compositionDataElement');
-    let [ns, de] = createNameSpaceAndDE('shr.test', 'composition');
-    de.description = 'It is a composition data element';
-    de.addComponent(new QuantifiedIdentifier(new Identifier('shr.test', 'simple'), 0, 1));
-    de.addComponent(new QuantifiedIdentifier(new Identifier('shr.test', 'coded'), 0));
+  it('should correctly export a group', () => {
+    let expectedSchema = importFixture('GroupOfThingsDataElement');
+    let [ns, de] = createNameSpaceAndDE('shr.test', 'GroupOfThings');
+    de.description = 'It is a group data element';
+    de.addComponent(new QuantifiedIdentifier(new Identifier('shr.test', 'Simple'), 0, 1));
+    de.addComponent(new QuantifiedIdentifier(new Identifier('shr.test', 'Coded'), 0));
     de.addComponent(new QuantifiedIdentifier(new Identifier('shr.test', 'Simple'), 1));
-    de.addComponent(new QuantifiedIdentifier(new Identifier('other.ns', 'thing'), 1, 1));
+    de.addComponent(new QuantifiedIdentifier(new Identifier('other.ns', 'Thing'), 1, 1));
     let schema = exportToSingleSchema(ns);
     expect(schema).to.eql(expectedSchema);
   });
@@ -71,13 +62,6 @@ describe('#exportToSchemas()', () => {
     expect(schemas).to.eql(expectedSchemas);
   });
 });
-
-function createNameSpaceAndEntry(namespace, name) {
-  let ns = new Namespace(namespace);
-  let entry = new Entry(new Identifier(namespace, name));
-  ns.addElement(entry);
-  return [ns, entry];
-}
 
 function createNameSpaceAndDE(namespace, name) {
   let ns = new Namespace(namespace);
