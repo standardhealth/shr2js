b027823dcaf3f872d158271306a32c3d30ff4530
Chris Moesel <cmoesel@mitre.org>
2016-11-01 13:40:32 -0400

    Refactor exporter tests & fix exporters
    Most exporters probably want to run the same basic tests.  Test code was refactored to support a common set of tests that can easily be used in different exporters.
    
    These common tests were applied to both schema and structdef exporters -- and uncovered some bugs / unimplemented features.  These issues have also been addressed.

--- test/schema/export_test.js
+++ test/schema/export_test.js
@@ -1,54 +1,12 @@
 const {expect} = require('chai');
 const fs = require('fs');
+const th = require('../test_helper');
 const {exportToSchemas} = require('../../lib/schema/export');
-const {Namespace, DataElement, Group, Identifier, QuantifiedValue, PrimitiveIdentifier, Value, CodeValue, OrValues} = require('../../lib/models');
+const {Namespace, DataElement, Identifier, PrimitiveIdentifier, Value} = require('../../lib/models');
 
-describe('#exportToSchemas()', () => {
-  it('should correctly export a simple data element', () => {
-    let expectedSchema = importFixture('SimpleDataElement');
-    let [ns, de] = createNameSpaceAndDE('shr.test', 'Simple');
-    de.description = 'It is a simple data element';
-    de.value = new Value(new PrimitiveIdentifier('string'));
-    let schema = exportToSingleSchema(ns);
-    expect(schema).to.eql(expectedSchema);
-  });
+describe('#exportToSchemasCommonCases()', th.commonTests(importFixture, exportNamespaces));
 
-  it('should correctly export a coded data element', () => {
-    let expectedSchema = importFixture('CodedDataElement');
-    let [ns, de] = createNameSpaceAndDE('shr.test', 'Coded');
-    de.description = 'It is a coded data element';
-    de.value = new CodeValue('http://standardhealthrecord.org/test/vs/Coded');
-    let schema = exportToSingleSchema(ns);
-    expect(schema).to.eql(expectedSchema);
-  });
-
-  it('should correctly export a choice data element', () => {
-    let expectedSchema = importFixture('ChoiceDataElement');
-    let [ns, de] = createNameSpaceAndDE('shr.test', 'Choice');
-    de.description = 'It is a data element with a choice';
-    let or = new OrValues();
-    or.addValue(new Value(new PrimitiveIdentifier('date')));
-    or.addValue(new Value(new Identifier('other.ns', 'Period')));
-    or.addValue(new Value(new Identifier('shr.test', 'Simple')));
-    de.value = or;
-    let schema = exportToSingleSchema(ns);
-    expect(schema).to.eql(expectedSchema);
-  });
-
-  it('should correctly export a group', () => {
-    let expectedSchema = importFixture('GroupOfThingsDataElement');
-    let ns = new Namespace('shr.test');
-    let de = new Group(new Identifier('shr.test', 'GroupOfThings'));
-    ns.addDefinition(de);
-    de.description = 'It is a group data element';
-    de.addElement(new QuantifiedValue(new Value(new Identifier('shr.test', 'Simple')), 0, 1));
-    de.addElement(new QuantifiedValue(new Value(new Identifier('shr.test', 'Coded')), 0));
-    de.addElement(new QuantifiedValue(new Value(new Identifier('shr.test', 'Choice')), 1));
-    de.addElement(new QuantifiedValue(new Value(new Identifier('other.ns', 'Thing')), 1, 1));
-    let schema = exportToSingleSchema(ns);
-    expect(schema).to.eql(expectedSchema);
-  });
-
+describe('#exportToSchemasUniqueCases()', () => {
   it('should correctly export all primitive types', () => {
     let ns = new Namespace('shr.test');
     for (let p of ['boolean', 'integer', 'decimal', 'unsignedInt', 'positiveInt',
@@ -59,26 +17,18 @@ describe('#exportToSchemas()', () => {
       de.value = new Value(new PrimitiveIdentifier(p));
       ns.addDefinition(de);
     }
-    let expectedSchemas = importFixture('primitives', '.json');
+    let expectedSchemas = importFixture('primitives');
     let schemas = exportToSchemas([ns]);
     expect(schemas).to.have.length(16);
     expect(schemas).to.eql(expectedSchemas);
   });
 });
 
-function createNameSpaceAndDE(namespace, name) {
-  let ns = new Namespace(namespace);
-  let de = new DataElement(new Identifier(namespace, name));
-  ns.addDefinition(de);
-  return [ns, de];
+function exportNamespaces(...namespace) {
+  let schemas = exportToSchemas(namespace);
+  return schemas;
 }
 
-function exportToSingleSchema(namespace) {
-  let schemas = exportToSchemas([namespace]);
-  expect(schemas).to.have.length(1);
-  return schemas[0];
-}
-
-function importFixture(name, ext='.schema.json') {
+function importFixture(name, ext='.json') {
   return JSON.parse(fs.readFileSync(`${__dirname}/fixtures/${name}${ext}`, 'utf8'));
 }
